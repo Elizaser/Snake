4b7b8109542e208f2274d02bf890c67623c84721
Elizaser <liza.gluhowerowa@gmail.com>
2020-11-13 17:13:31 +0700

    Class Point

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,8 +10,26 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sum = '*';
+            p1.Draw();
+
+            //int x1 = 1, y1 = 3;
+            //char sum1 = '*';
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sum = '#';
+            p2.Draw();
+
+            //int x2 = 4, y2 = 5;
+            //char sum2 = '#';
+
             Console.ReadLine();
         }
+
+
     }
 }
