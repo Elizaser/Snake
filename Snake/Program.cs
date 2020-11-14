142f870c72a49d36f388b3bbdda4ab6339d07893
Elizaser <liza.gluhowerowa@gmail.com>
2020-11-14 15:08:50 +0700

    Event handling

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -14,37 +14,53 @@ namespace Snake
             HorizontalLine lineUp = new HorizontalLine(0, 117, 0, '+');
             lineUp.Drow();
 
-            HorizontalLine lineDown = new HorizontalLine(0, 117, 29, '+');
+            HorizontalLine lineDown = new HorizontalLine(0, 117, 28, '+');
             lineDown.Drow();
             
-            VerticalLine lineLeft = new VerticalLine(0, 0, 29, '+');
+            VerticalLine lineLeft = new VerticalLine(0, 0, 28, '+');
             lineLeft.Drow();
 
 
-            VerticalLine lineRight = new VerticalLine(118, 0, 29, '+');
+            VerticalLine lineRight = new VerticalLine(118, 0, 28, '+');
             lineRight.Drow();
 
             Point p = new Point(4, 5, '*');
 
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
 
-            Console.ReadLine();
+            while(true)
+            {
+                Console.CursorVisible = false;
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
+
+
+
+
+            //snake.Move();
+            //Thread.Sleep(300);
+            //snake.Move();
+            //Thread.Sleep(300);
+            //snake.Move();
+            //Thread.Sleep(300);
+            //snake.Move();
+            //Thread.Sleep(300);
+            //Thread.Sleep(300);
+            //snake.Move();
+            //Thread.Sleep(300);
+            //snake.Move();
+            //Thread.Sleep(300);
+            //snake.Move();
+            //Thread.Sleep(300);
+
+            //Console.ReadLine();
         }
 
 
